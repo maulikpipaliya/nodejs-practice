21804e110d49479d3c09267d0627db19e674bb31
maulikpipaliya <maulik.pipaliya@gmail.com>
2020-01-10 10:44:32 +0530

    S7-45 Serve Static Assets

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,16 +1,23 @@
 const express = require('express');
+const path = require('path')
 
 const app = express();
 const port = 3000;
 
-app.get('',(request, response)=>{
-    console.log(request);
-    response.send('Hello Joyy! It\'s cool');
-});
+console.log(__dirname);
+const publicDirPath = path.join(__dirname,'../public')
+console.log(publicDirPath);
 
-app.get('/help', (request, response) => {
-    response.send('<h1>Help page</h1>')
-})
+app.use(express.static(publicDirPath)); // comes from static file.
+
+// app.get('',(request, response)=>{
+//     console.log(request);
+//     response.send('Hello Joyy! It\'s cool');
+// });
+
+// app.get('/help', (request, response) => {
+//     response.send('<h1>Help page</h1>')
+// })
 
 app.get('/about', (request, response) => {
     response.send('<h3> About page </h3> ')
