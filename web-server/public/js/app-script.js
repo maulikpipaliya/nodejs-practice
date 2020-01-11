420c1fa7ac44ac7f5a4323835753df2f034d0e7f
maulikpipaliya <maulik.pipaliya@gmail.com>
2020-01-11 19:32:53 +0530

    S8-59 Wired up UI

--- web-server/public/js/app-script.js
+++ web-server/public/js/app-script.js
@@ -4,24 +4,36 @@ console.log("Javascript from the file has been loaded.");
 
 
 const searchBox = document.querySelector('input');
-
 const weatherForm = document.querySelector('form');
+const msg1 = document.querySelector('#search-results');
+const msg2 = document.querySelector('#search-results-error');
+
+msg1.textContent = '';
+
 weatherForm.addEventListener('submit', (e) => {
     e.preventDefault();
     const input = searchBox.value;
     console.log(input);
 
 
-    
-    fetch('http://localhost:3000/weather?address=' + input).then((response) => {
+    msg1.textContent = 'Loading message';
+    msg2.textContent = '';
+
+    fetch('http://localhost:3000/weather?address=' + input  ).then((response) => {
         response.json().then((data) => {
             if (data.error) {
                 console.log(data.error);
+                msg1.textContent = data.error;
+                msg2.textContent = '';
+                
             }
             else {
                 console.log(data);
+                msg1.textContent = 'For location: ' + data.location + ', temperature is ' + data.temperature + '. ';
+                msg2.textContent += data.summary;
             }
         })
     })
     // return false;
 })
+
