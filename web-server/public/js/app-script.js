b8b81fc025af34f96376158f2c36545f76015eed
maulikpipaliya <maulik.pipaliya@gmail.com>
2020-01-11 19:12:53 +0530

    S8-58 Created a Search Form

--- web-server/public/js/app-script.js
+++ web-server/public/js/app-script.js
@@ -1,14 +1,27 @@
 console.log("Javascript from the file has been loaded.");
 
 
-const search = 'Mumbai India';
-fetch('http://localhost:3000/weather?address=!').then((response) => {
-    response.json().then((data) => {
-        if(data.error){
-            console.log(data.error);
-        }
-        else{
-            console.log(data);
-        }
+
+
+const searchBox = document.querySelector('input');
+
+const weatherForm = document.querySelector('form');
+weatherForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const input = searchBox.value;
+    console.log(input);
+
+
+    
+    fetch('http://localhost:3000/weather?address=' + input).then((response) => {
+        response.json().then((data) => {
+            if (data.error) {
+                console.log(data.error);
+            }
+            else {
+                console.log(data);
+            }
+        })
     })
+    // return false;
 })
